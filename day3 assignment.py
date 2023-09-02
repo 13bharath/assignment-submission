dba67479fa54fb1051f3a06413a0118825062f56
13bharath <143818844+13bharath@users.noreply.github.com>
2023-09-02 19:35:59 +0530

    Add files via upload

---
+++ day3 assignment.py
@@ -0,0 +1,5 @@
+#for finding area of square using function
+def area(side):
+    return side*side
+side=int(input("enter the side length of the square:"))
+print("the area of the squareis:",area(side))
